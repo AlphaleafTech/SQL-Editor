079895f3b8b78d3d5f38cc4854885632725a28c3
jaagrav <jaagravseal03@gmail.com>
2022-06-07 02:42:50 +0530

    add: install useful libraries and setup tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      darkTextColor: "#FFD66D",
+      darkBGColor: "#262626",
+      darkSecondaryBGColor: "#424242",
+      white: "#ffffff",
+    },
+    extends: {},
+  },
+  plugins: [],
+};
