02a9a514c98a5bc5b80f0984ea200c44a0055c15
jaagrav <jaagravseal03@gmail.com>
2022-06-08 02:37:22 +0530

    add: separate table component and file for convert function to reduce redundancy

--- components/CurrentTable/index.js
+++ components/CurrentTable/index.js
@@ -2,24 +2,13 @@ import { useState, useEffect } from "react";
 
 import DefaultCSVData from "../../public/users.csv";
 
+import { convert, Table } from "../";
+
 export default function CurrentTable({ setBody }) {
   const [csvData, setCSVData] = useState(""),
     [tableData, setTableData] = useState(null);
 
-  const convert = (data, mode) => {
-    switch (mode) {
-      case 1:
-        data = data.split("\n");
-        data = data.map((line) => line.split(","));
-        return data;
-      case 2:
-        data = data.map((line) => line.join(","));
-        data = data.join("\n");
-        return data;
-    }
-  };
-
-  function readImage(file) {
+  const readCSV = (file) => {
     if (file.target.value && !file.target.value.endsWith(".csv")) {
       return;
     }
@@ -28,14 +17,14 @@ export default function CurrentTable({ setBody }) {
     reader.addEventListener("load", (event) => {
       let data = event.target.result;
       setCSVData(data);
-      setTableData(convert(data, 1));
+      setTableData(convert(data, 1)); // Convert CSV into table
     });
     reader.readAsText(file.target.files[0]);
-  }
+  };
 
   useEffect(() => {
     setTableData(DefaultCSVData);
-    setCSVData(convert(DefaultCSVData, 2));
+    setCSVData(convert(DefaultCSVData, 2)); // Store default Table data as CSV
   }, []);
 
   useEffect(() => {
@@ -47,6 +36,7 @@ export default function CurrentTable({ setBody }) {
       <div className="flex justify-between items-center">
         <div className="text-darkBGColor dark:text-darkTextColor text-xl">
           CurrentTable
+          <span className={"block text-sm"}>Name of the Table</span>
         </div>
         <div className="flex justify-between items-center gap-2">
           <label className="text-sm text-darkBGColor dark:text-darkTextColor ">
@@ -56,7 +46,7 @@ export default function CurrentTable({ setBody }) {
             className="w-48 block text-sm text-gray-900 bg-gray-50 border cursor-pointer dark:text-darkTextColor outline-none border-lightSecondaryColor dark:border-darkSecondaryBGColor dark:placeholder-darkTextColor p-2"
             type="file"
             content="Upload CSV"
-            onChange={readImage}
+            onChange={readCSV}
             accept={".csv"}
             aria-describedby="file_input_help"
             id="file_input"
@@ -64,22 +54,7 @@ export default function CurrentTable({ setBody }) {
         </div>
       </div>
       <div className="w-full h-full flex justify-start overflow-auto border border-darkBGColor/25 dark:border-white/25">
-        <table className="w-full text-darkBGColor dark:text-darkTextColor">
-          {tableData &&
-            tableData.map((item, index) => (
-              <tr key={`tr-${index}`}>
-                {item &&
-                  item.map((item, index) => (
-                    <td
-                      className="border border-darkBGColor/25 dark:border-white/25 p-2"
-                      key={`td-${index}`}
-                    >
-                      {item}
-                    </td>
-                  ))}
-              </tr>
-            ))}
-        </table>
+        <Table data={tableData} />
       </div>
     </div>
   );
