a4b3c9317cf24bf6b4d611a5439aed9dccaca9b0
jaagrav <jaagravseal03@gmail.com>
2022-06-07 21:05:13 +0530

    fix: window is not defined error thrown by acejs

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
 import Image from "next/image";
-import { Header, Panel } from "../components";
+import { Header, Panel, Editor } from "../components";
 
-import dynamic from "next/dynamic";
-
-const Editor = dynamic(() => import("../components/Editor"), { ssr: false });
 export default function Home() {
   return (
     <div className="h-screen w-full grid grid-rows-[auto_50vh_1fr]">
